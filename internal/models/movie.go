3f875da96a673c56540ba3e08cbe283267cf3987
Artyom <mrfortsky@gmail.com>
2026-02-09 11:13:09 +0300

    Update db fields and model structs

--- internal/models/movie.go
+++ internal/models/movie.go
@@ -2,12 +2,26 @@ package models
 
 import "time"
 
+// All avaible genres here
+var AvailableGenres = []string{
+	"Боевик", "Приключения", "Мультфильм", "Биография", "Комедия", "Криминал",
+	"Документальный", "Драма", "Семейный", "Фэнтези", "Исторический", "Ужасы",
+	"Музыка", "Мюзикл", "Детектив", "Мелодрама", "Научная фантастика", "Спорт",
+	"Триллер", "Военный", "Вестерн",
+}
+
 // Main struct to interract with db
 type Movie struct {
-	ID         int       `json:"id"`
-	Name       string    `json:"name"`
-	PictureKey string    `json:"picture_key"`
-	CreatedAt  time.Time `json:"created_at"`
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	PictureKey  string    `json:"picture_key"`
+	Genres      []string  `json:"genres"`
+	Year        int       `json:"year"`
+	Description string    `json:"description"`
+	Rating      int       `json:"rating"`
+	WorldRating float32   `json:"world_rating"`
+	Comment     string    `json:"comment"`
+	CreatedAt   time.Time `json:"created_at"`
 }
 
 // What the user will send to us through form
@@ -17,9 +31,15 @@ type MovieCreateRequest struct {
 	Picture []byte `json:"-" form:"picture"`
 }
 
-// What we'll send back to the user
+// What we'll send back to the user (includes picture)
 type MovieResponse struct {
-	ID         int    `json:"id"`
-	Name       string `json:"name"`
-	PictureURL string `json:"picture_url"`
+	ID          int      `json:"id"`
+	Name        string   `json:"name"`
+	PictureURL  string   `json:"picture_url"`
+	Genres      []string `json:"genres"`
+	Year        int      `json:"year"`
+	Description string   `json:"description"`
+	Rating      int      `json:"rating"`
+	WorldRating float32  `json:"world_rating"`
+	HasComment  bool     `json:"has_comment"`
 }
